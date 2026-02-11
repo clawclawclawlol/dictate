1a9f3e1c8a25f83522fb2f7c1f3a6a6f128fc9d1
Yuri <yurimoraxo@gmail.com>
2026-02-11 18:18:31 +0000

    bump: v2.4.1 - paste spacing fix

--- dictate/__init__.py
+++ dictate/__init__.py
@@ -4,7 +4,7 @@ Dictate - Push-to-Talk Voice Dictation for macOS
 A local, privacy-first voice dictation menu bar app using Apple Silicon MLX models.
 """
 
-__version__ = "2.4.0"
+__version__ = "2.4.1"
 __author__ = "brando"
 
 from dictate.config import Config
